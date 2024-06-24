ffcb60be78e2294fa9a4d86e40b1ac9afdcdde5b
Chenghui Li <cl2228@cornell.edu>
2024-06-23 21:38:40 -0700

    Finish Chapter 2.
    1. Tokenization.
    2. Sliding window dataset creation.
    3. BPE, byte pair encoding for efficient unknown word handling.
    4. Embedding layer in PyTorch as a lookup "table".
    5. Positional embedding for position context differentiation.

--- src/pre_processing/test.py
+++ src/pre_processing/test.py
@@ -2,3 +2,10 @@
 
 if __name__ == '__main__':
     print("this is a preprocessing directory")
+
+    a = [i for i in range(20)]
+    for i in range(0, len(a) - 4, 1):
+        print(i)
+        # i=15
+        print(a[i: i + 4])
+        print(a[i + 1: i + 1 + 4])
